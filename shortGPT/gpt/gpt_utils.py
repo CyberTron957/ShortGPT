871d259fdec35c4b55f155d92886bdd56deada41
tron957 <84897133+CyberTron957@users.noreply.github.com>
2024-10-16 14:13:52 +0530

    Update gpt_utils.py

--- shortGPT/gpt/gpt_utils.py
+++ shortGPT/gpt/gpt_utils.py
@@ -70,8 +70,8 @@ def open_file(filepath):
 
 
 def gpt3Turbo_completion(chat_prompt="", system="You are an AI that can give the answer to anything", temp=0.7, model="gpt-3.5-turbo", max_tokens=1000, remove_nl=True, conversation=None):
-    openai.api_base = os.getenv("OPENAI_BASE_URL", "https://api.naga.ac/v1")  
-    openai.api_key = ApiKeyManager.get_api_key("OPENAI")
+    client = OpenAI(base_url='https://api.naga.ac/v1', api_key='ng-LGcMxBTm67vhTuchGZMthJ3gJxb5L')
+
     max_retry = 5
     retry = 0
     while True:
@@ -83,7 +83,7 @@ def gpt3Turbo_completion(chat_prompt="", system="You are an AI that can give the
                     {"role": "system", "content": system},
                     {"role": "user", "content": chat_prompt}
                 ]
-            response = openai.chat.completions.create(
+            response = client.chat.completions.create(
                 model=model,
                 messages=messages,
                 max_tokens=max_tokens,
